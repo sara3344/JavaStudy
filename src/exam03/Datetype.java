9c8f646e897117285c8a783944a9b3e765da5816
jen3326 <kisss44@naver.com>
2021-08-27 00:42:09 +0900

    add1

---
+++ src/exam03/Datetype.java
@@ -0,0 +1,19 @@
+package exam03;
+
+public class Datetype {
+    public static void main(String[] args) {
+        System.out.println(6); // NUmber
+        System.out.println("Six"); // String
+
+        System.out.println("6"); // String 6
+
+        System.out.println(6 + 6); // 12
+        System.out.println("6" + "6"); // 66
+
+        System.out.println("6*6"); // 36
+//        System.out.println("6"*"6"); 에러
+
+        System.out.println("1111".length()); // 4 문자 길이
+//        System.out.println(1111.length()); 숫자 놉
+    }
+}
