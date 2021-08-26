1ae2a56634a74a332b968ae8f9f26e5e5ecc151d
jen3326 <kisss44@naver.com>
2021-08-27 00:45:03 +0900

    add1

---
+++ src/exam03/Number.java
@@ -0,0 +1,15 @@
+package exam03;
+
+public class Number {
+    public static void main(String[] args) {
+        // 연산자 Operator
+        System.out.println(6 + 2); // 8
+        System.out.println(6 - 2); // 4
+        System.out.println(6 * 2); // 12
+        System.out.println(6 / 2); // 3
+
+        System.out.println(Math.PI); // 3.141592
+        System.out.println(Math.floor(Math.PI)); // 내림
+        System.out.println(Math.ceil(Math.PI)); // 올림
+    }
+}
---
+++ src/exam03/StringApp.java
@@ -0,0 +1,20 @@
+package exam03;
+
+public class StringApp {
+    public static void main(String[] args) {
+
+        //Character Bs String
+        System.out.println("Hello World"); // String (캐릭터들이 모여있는 거)
+        System.out.println('H'); // Character 작은 따옴표와 큰 따옴표는 다름
+        System.out.println("H");
+
+        System.out.println("Hello " +
+                "World"); // 줄바꿈 놉
+
+        //new line
+        System.out.println("Hello \nWorld"); // \n 줄바꿈
+
+        // escape
+        System.out.println("Hello \"World\""); // Hello "World"
+    }
+}
