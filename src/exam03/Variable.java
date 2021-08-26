f223c934d36e4871df06d047cb34527cc228853f
jen3326 <kisss44@naver.com>
2021-08-27 00:54:59 +0900

    add1

---
+++ src/exam03/Variable.java
@@ -0,0 +1,15 @@
+package exam03;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1; // number -> integer(정수)
+        System.out.println(a);
+
+        double b = 1.1; // real number -> double
+        System.out.println(b);
+
+        String c = "Hello World";
+        System.out.println(c
+        );
+    }
+}
