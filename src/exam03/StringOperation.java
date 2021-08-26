20920c0f0a72a79674e5d50964704ac35c05b1d4
jen3326 <kisss44@naver.com>
2021-08-27 00:46:15 +0900

    add1

--- src/exam03/StringOperation.java
+++ src/exam03/StringOperation.java
@@ -3,7 +3,7 @@ package exam03;
 public class StringOperation {
     public static void main(String[] args) {
 
-        System.out.println("Hello World".length()); //
+        System.out.println("Hello World".length()); // 11
         System.out.println("Hello, leezche ... bye. ".replace("leezche", "egoing"));
         System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "egoing"));
     }
