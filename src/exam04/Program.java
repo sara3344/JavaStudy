f6db3eb88d62ca1ee187426fbc61e1d220c9f88c
jen3326 <kisss44@naver.com>
2021-08-27 22:46:30 +0900

    add1

--- src/exam04/Program.java
+++ src/exam04/Program.java
@@ -2,8 +2,8 @@ package exam04;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
+        System.out.println(1); // 이런 코드가 1억개 있다고 극단적으로 생각해보기
+        System.out.println(2); // 절레 절레
         System.out.println(3);
     }
 }
