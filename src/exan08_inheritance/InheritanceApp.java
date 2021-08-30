32c832469cd0136b873d97c0b2e293ce628d7ee9
jen3326 <kisss44@naver.com>
2021-08-31 01:59:01 +0900

    상속과 생성자

--- src/exan08_inheritance/InheritanceApp.java
+++ src/exan08_inheritance/InheritanceApp.java
@@ -1,49 +1,37 @@
 package exan08_inheritance;
 
 class Cal {
-    public int sum(int v1, int v2) {
-        return v1 + v2;
+    int v1, v2;
+
+    Cal(int v1, int v2) {
+        System.out.println("Cal init!!");
+        this.v1 = v1;
+        this.v2 = v2;
     }
 
-    // Overloading
-    public int sum(int v1, int v2, int v3) {
-        return this.sum(v1, v2) + v3;
+    public int sum() {
+        return this.v1 + v2;
     }
 }
 
 class Cal3 extends Cal {
-    public int minus(int v1, int v2) {
-        return v1 - v2;
+
+    Cal3(int v1, int v2) {
+        super(v1, v2);
+        System.out.println("Cal3 init!!");
     }
 
-    // Overriding
-    public int sum(int v1, int v2) {
-        System.out.println("Cal3!!");
-        return super.sum(v1, v2);
+    public int minus() {
+        return this.v1 - v2;
     }
 }
 
 public class InheritanceApp {
     public static void main(String[] args) {
-
-        Cal c = new Cal();
-        System.out.println(c.sum(2, 1));
-        System.out.println(c.sum(2, 1, 1));
-
-        Cal3 c3 = new Cal3();
-        System.out.println(c3.sum(2, 1));
-        System.out.println(c3.minus(2, 1));
-        System.out.println(c3.sum(2, 1));
-
-    }
-}
-
-class Ca12 {
-    public int sum(int v1, int v2) {
-        return v1 + v2;
-    }
-
-    public int minus(int v1, int v2) {
-        return v1 - v2;
+        Cal c = new Cal(2, 1);
+        Cal3 c3 = new Cal3(2, 1);
+        System.out.println((c3.sum())); // 3
+        System.out.println((c3.minus())); // 1
+        ;
     }
 }
