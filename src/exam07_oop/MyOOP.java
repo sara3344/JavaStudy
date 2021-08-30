7351b12360493807d86cf0bf4ef7d4abeaa28f2b
jen3326 <kisss44@naver.com>
2021-08-30 22:39:46 +0900

    add1

--- src/exam07_oop/MyOOP.java
+++ src/exam07_oop/MyOOP.java
@@ -1,21 +1,5 @@
 package exam07_oop;
 
-class Print {
-    public static String delimiter = "";
-
-    public static void A() {
-        System.out.println(delimiter);
-        System.out.println("A");
-        System.out.println("A");
-    }
-
-    public static void B() {
-        System.out.println(delimiter);
-        System.out.println("B");
-        System.out.println("B");
-    }
-}
-
 public class MyOOP {
     public static void main(String[] args) {
         Print.delimiter = "----";
