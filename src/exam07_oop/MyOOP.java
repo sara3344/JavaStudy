943e503410103b3d47166d0de1d72064de43df58
jen3326 <kisss44@naver.com>
2021-08-30 16:48:21 +0900

    add1

---
+++ src/exam07_oop/MyOOP.java
@@ -0,0 +1,22 @@
+package exam07_oop;
+
+public class MyOOP {
+    public static void main(String[] args) {
+        // ..
+        printA();
+        printA();
+        printB();
+        printB();
+    }
+
+    public static void printA() {
+        System.out.println("----");
+        System.out.println("A");
+        System.out.println("A");
+    }
+    public static void printB() {
+        System.out.println("----");
+        System.out.println("B");
+        System.out.println("B");
+    }
+}
