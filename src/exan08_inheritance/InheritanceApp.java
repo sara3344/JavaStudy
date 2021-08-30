2714cffb27d9d2f6dee0ed7c2b14c9c1350e50dd
jen3326 <kisss44@naver.com>
2021-08-31 01:43:54 +0900

    Overriding & Overloading

--- src/exan08_inheritance/InheritanceApp.java
+++ src/exan08_inheritance/InheritanceApp.java
@@ -4,15 +4,19 @@ class Cal {
     public int sum(int v1, int v2) {
         return v1 + v2;
     }
+    // Overloading
+    public int sum(int v1, int v2, int v3) {
+        return v1 + v2 + v3;
+    }
 }
 
 class Cal3 extends Cal {
-    public int minus(int v1, int v2) { // 부모 클래스가 갖고 있지 않은 메소드 추가
+    public int minus(int v1, int v2) {
         return v1 - v2;
     }
 
     // Overriding
-    public int sum(int v1, int v2) { // 부모 클래스가 갖고 있는 메소드 재정의
+    public int sum(int v1, int v2) {
         System.out.println("Cal3!!");
         return v1 + v2;
     }
@@ -23,6 +27,7 @@ public class InheritanceApp {
 
         Cal c = new Cal();
         System.out.println(c.sum(2, 1));
+        System.out.println(c.sum(2, 1, 1));
 
         Cal3 c3 = new Cal3();
         System.out.println(c3.sum(2, 1));
