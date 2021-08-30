b9ab734e28687065f98e37e208d54e2ec66f5538
jen3326 <kisss44@naver.com>
2021-08-31 01:09:07 +0900

    this

--- src/exam07_oop/AccountingApp.java
+++ src/exam07_oop/AccountingApp.java
@@ -3,6 +3,9 @@ package exam07_oop;
 class Accounting {
     public double valueOfSupply;
     public static double vatRate = 0.1; // 클래스 소속
+    public Accounting(double valueOfSupply) {
+        this.valueOfSupply = valueOfSupply;
+    }
 
     public double getVAT() {
         return valueOfSupply * vatRate;
@@ -15,11 +18,9 @@ class Accounting {
 
 public class AccountingApp {
     public static void main(String[] args) {
-        Accounting a1 = new Accounting();
-        a1.valueOfSupply = 10000.0;
+        Accounting a1 = new Accounting(10000.0);
 
-        Accounting a2 = new Accounting();
-        a2.valueOfSupply = 20000.0;
+        Accounting a2 = new Accounting(20000.0);
 
         System.out.println("Value of supply: " + a1.valueOfSupply);
         System.out.println("Value of supply: " + a2.valueOfSupply);
