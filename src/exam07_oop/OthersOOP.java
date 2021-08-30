7f22cab0000a54244860d9890cb186d2090cd483
jen3326 <kisss44@naver.com>
2021-08-30 16:34:25 +0900

    add1

--- src/exam07_oop/OthersOOP.java
+++ src/exam07_oop/OthersOOP.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 
 public class OthersOOP {
     public static void main(String[] args) throws IOException {
+        // class : System, Math, FileWriter
+        // instance : f1, f2
 
         // Math라는 클래스 안에 아래와 같은 변수들이 있다
         System.out.println(Math.PI);
@@ -15,5 +17,10 @@ public class OthersOOP {
         f1.write("Hello");
         f1.write(" Java");
         f1.close();
+
+        FileWriter f2 = new FileWriter("data2.txt");
+        f2.write("Hello");
+        f2.write(" Java2");
+        f2.close();
     }
 }
